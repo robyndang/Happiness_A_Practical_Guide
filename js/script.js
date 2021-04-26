389d38a7a16bab85aa0b7827c87ad034f880adfb
robynloudang <robyn.dang@hotmail.ca>
2021-04-25 21:10:49 -0400

    update

--- js/script.js
+++ js/script.js
@@ -49,27 +49,29 @@ faders.forEach(fader => {
 });
 
 /* ANIMATE VIDEO ON SCROLL */
+var mq = window.matchMedia( "(min-width: 1024px)" );
+if (mq.matches) {
+  var frameNumber = 0, // start video at frame 0
+      // lower numbers = faster playback
+      playbackConst = 500,
+      // get page height from video duration
+      setHeight = document.getElementById("set-height"),
+      // select video element
+      vid = document.getElementById('v0');
+      // var vid = $('#v0')[0]; // jquery option
 
-var frameNumber = 0, // start video at frame 0
-    // lower numbers = faster playback
-    playbackConst = 500,
-    // get page height from video duration
-    setHeight = document.getElementById("set-height"),
-    // select video element
-    vid = document.getElementById('v0');
-    // var vid = $('#v0')[0]; // jquery option
-
-// dynamically set the page height according to video length
-vid.addEventListener('loadedmetadata', function() {
-  setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
-});
+  // dynamically set the page height according to video length
+  vid.addEventListener('loadedmetadata', function() {
+    setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
+  });
 
 
-// Use requestAnimationFrame for smooth playback
-function scrollPlay(){
-  var frameNumber  = window.pageYOffset/playbackConst;
-  vid.currentTime  = frameNumber;
+  // Use requestAnimationFrame for smooth playback
+  function scrollPlay(){
+    var frameNumber  = window.pageYOffset/playbackConst;
+    vid.currentTime  = frameNumber;
+    window.requestAnimationFrame(scrollPlay);
+  }
+
   window.requestAnimationFrame(scrollPlay);
-}
-
-window.requestAnimationFrame(scrollPlay);
+  }
